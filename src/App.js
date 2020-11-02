bbc3fbb12df7b49b76ccbc92f486c7b98d72ac39
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-02 12:44:08 -0600

    updated favicon and made a goofy spinning logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import roysterLogo from './royster svg.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={roysterLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's do this :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Roysters NC
+        </h1>
       </header>
     </div>
   );
