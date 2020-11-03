6271b140662e78c209b8f28f4af609fbe6098820
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-03 09:52:56 -0600

    building weather component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 // import logo from './logo.svg';
-import roysterLogo from './royster svg.svg';
-import './App.css';
-import Weather from '../src/components/Weather/Weather'
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={roysterLogo} className="App-logo" alt="logo" />
-        <p>
-          Let's do this :)
-        </p>
-        <h1>
-          Roysters NC
-        </h1>
-        <Weather />
-      </header>
-    </div>
-  );
+import roysterLogo from "./royster svg.svg";
+import "./App.css";
+import Weather from "../src/components/Weather/Weather";
+
+class App extends Component {
+constructor (props) {
+  super(props)
+  console.log('[App.js] is rendering')
+}
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={roysterLogo} className="App-logo" alt="logo" />
+          <p>Let's do this :)</p>
+          <h1>Roysters NC</h1>
+          <Weather getWeather={this.getCurrent} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,21 +1,20 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import './Weather.css'
+require('dotenv').config();
+
+
+
 
 class weather extends Component  {
-    async getCurrent() {
-    const apiKey = 'b85047ff65336fa006bc7a89fd300ed3';
-    const lat = 34.789556;
-    const lon = -76.601258;
 
-    const response = await fetch(
-`        api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-    const data = await response.json();
-    console.log(data);
-    return data;
-    }
+  
+   
     render (data) {
+        console.log('weather is rendering')
         return (
-            <div>
-                <h5></h5>
+            <div className='weatherBox'>
+               <p className="temp">15 degrees</p>
+               <p className="weatherDescription">Cloudy</p>
             </div>
         )
     }
