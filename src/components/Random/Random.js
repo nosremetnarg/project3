5a28b519ef75592bd3051d01655587384e77e346
Grant Emerson <grant.alex.emerson@gmail.com>
2020-11-05 12:50:06 -0600

    random quotes added and new photos added

--- src/components/Random/Random.js
+++ src/components/Random/Random.js
@@ -17,12 +17,16 @@ function Random() {
     "One Oyster can filter 50 gallons of water a day.",
     "Oysters absorb 76-93% of wave energy, which reduces erosion, and flooding during storms.",
     "Thanks to modern food safety, the 'R' policy no longer applies.",
-    "Why don't oysters share their pearls? Because they are shellfish",
-    "What is an oyster's favorite band? Pearl Jam",
-    'An oyster can filter 1.3 gallons of water per hour.'
+    "Why don't oysters share their pearls? Because they are shellfish.",
+    "What is an oyster's favorite band? Pearl Jam.",
+    'An oyster can filter 1.3 gallons of water per hour.',
+    'It’s not so much the species, but the location that gives oysters their flavor.',
+    'Oysters have been around for approximately 15 million years.',
+    'Oysters are vegetarians. They eat algae by filtering it out of the water.',
+    'Most Oysters Found in Restaurants Are Farmed.'
   ];
   function myTimer() {
-    let randomNum = [Math.floor(Math.random() * Math.floor(6))];
+    let randomNum = [Math.floor(Math.random() * Math.floor(randomFacts.length))];
     console.log(randomNum);
 console.log(randomFacts[randomNum]);
     let randomQuote = (randomFacts[randomNum]);
